9d1d2a4ca1444ab551d77a82d84e983c7bc1b842
Drazen Pavic <pavic@agentur22.de>
2021-04-09 14:00:43 +0200

    index

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<html>
+    <head>
+        <title>Pavic Docker PlayGround</title>
+    </head>
+    <body>
+        <h1>
+            Kakoje Pavic,
+        </h1>
+        <p>
+            Lorem idlodl ldoeor o ldeo, eodl lerooel!
+        </p>
+    </body>
+</html>
+
