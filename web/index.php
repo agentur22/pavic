0ac35fd4807f48bc4f1740dfb7351a9cb3183b51
Ivan <ivan.k@live.de>
2021-04-14 17:15:38 +0200

    css session mit dp

--- web/index.php
+++ web/index.php
@@ -10,19 +10,32 @@
        <div class="grid-container">
 <header>
     <div class="grid-container">
-        <div class="grid-x logo">
-            <div class="cell logo-box">nu</div>
-            <div class="cell logo-text">nuntium.</div>
-        </div>
-        <nav>
-            <ul class="main-nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Tags</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
-        </nav>
-        <div class="nav-right">
-            <a href="#" class="btn-login">Login</a>
+        <div class="grid-x">
+            <div class="cell shrink">
+                <div class="logo">
+                    <img src="assets/img/logo.svg" alt="Logo" />
+                </div>
+
+            </div>
+            <div class="cell shrink">
+
+                <nav class="main-nav">
+                    <ul>
+                        <li><a class="active" href="#">Home</a></li>
+                        <li><a href="#">Tags</a></li>
+                        <li><a href="#">About</a></li>
+                    </ul>
+                </nav>
+
+
+
+            </div>
+            <div class="cell auto">
+                <div class="nav-right">
+                    <div class="search"></div>
+                    <a href="#" class="btn-login">Login</a>
+                </div>
+            </div>
         </div>
     </div>
 </header>
@@ -30,10 +43,10 @@
     <div class="grid-container">
         <div class="hero-section">
             <img src="assets/img/article_image.png" alt="Hero image">
-            <div class="hero-text-box">
+            <div class="hero-text-box article">
                 <div class="article-tag">FEATURE ARTICLE</div>
                 <h2>World’s Most Dangerous Technology Ever Made.</h2>
-                <div class="article-meta">Ralph Hawkins * May 7, 2019 (10 mins read)</div>
+                <div class="article-meta">Ralph Hawkins • <span>May 7, 2019 (10 mins read)</span></div>
                 <div class="article-text">Proident aliquip velit qui commodo officia qui consectetur dolor ullamco
                     aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint
                     consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla
@@ -48,55 +61,68 @@
     <div class="grid-container">
         <div class="grid-x">
             <div class="cell brick_news_list large-8 large-offset-2">
-                <h1>editors prick</h1>
-                <div class="grid-x grid-margin-x">
-                    <div class="cell medium-6  large-4">
-                        <img src="assets/img/article1.png" alt="">
-                    </div>
-                    <div class="cell medium-6  large-8">
-                        <h2>
-                            Culpa sit Laboris Exercitation ea Fugiat
-                        </h2>
-                        <p>
-                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut
-                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit.
-                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
-                            ullamco labore et esse.
-                        </p>
-                    </div>
-                </div>
-                <div class="grid-x grid-margin-x">
-                    <div class="cell medium-6 large-4">
-                        <img src="assets/img/article2.png" alt="">
-                    </div>
-                    <div class="cell medium-6 large-8">
-                        <h2>
-                            Culpa sit Laboris Exercitation ea Fugiat
-                        </h2>
-                        <p>
-                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut
-                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit.
-                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
-                            ullamco labore et esse.
-                        </p>
+                <h1 class="border-headline">Quisque libero metus condimentum nec</h1>
+                <div class="news-article">
+                    <div class="grid-x grid-margin-x">
+                        <div class="cell medium-6  large-4">
+                            <img src="assets/img/article1.png" alt="">
+                        </div>
+                        <div class="cell medium-6  large-8">
+                            <div class="article">
+                                <div class="article-tag">FEATURE ARTICLE</div>
+                                <h2>World’s Most Dangerous Technology Ever Made.</h2>
+                                <div class="article-meta">Ralph Hawkins • <span>May 7, 2019 (10 mins read)</span></div>
+                                <div class="article-text">Proident aliquip velit qui commodo officia qui consectetur dolor ullamco
+                                    aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint
+                                    consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla
+                                    consequat velit magna.
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="grid-x grid-margin-x">
-                    <div class="cell medium-6 large-4">
-                        <img src="assets/img/article3.png" alt="">
-                    </div>
-                    <div class="cell medium-6 large-8">
-                        <h2>
-                            Culpa sit Laboris Exercitation ea Fugiat
-                        </h2>
-                        <p>
-                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut
-                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit.
-                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
-                            ullamco labore et esse.
-                        </p>
+                <div class="news-article">
+                    <div class="grid-x grid-margin-x">
+                        <div class="cell medium-6 large-4">
+                            <img src="assets/img/article2.png" alt="">
+                        </div>
+                        <div class="cell medium-6 large-8">
+                            <div class="article">
+                                <div class="article-tag">FEATURE ARTICLE</div>
+                                <h2>World’s Most Dangerous Technology Ever Made.</h2>
+                                <div class="article-meta">Ralph Hawkins • <span>May 7, 2019 (10 mins read)</span></div>
+                                <div class="article-text">Proident aliquip velit qui commodo officia qui consectetur dolor ullamco
+                                    aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint
+                                    consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla
+                                    consequat velit magna.
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <div class="news-article">
+                    <div class="grid-x grid-margin-x">
+                        <div class="cell medium-6 large-4">
+                            <img src="assets/img/article3.png" alt="">
+                        </div>
+                        <div class="cell medium-6 large-8">
+                            <div class="article">
+                                <div class="article-tag">FEATURE ARTICLE</div>
+                                <h2>World’s Most Dangerous Technology Ever Made.</h2>
+                                <div class="article-meta">Ralph Hawkins • <span>May 7, 2019 (10 mins read)</span></div>
+                                <div class="article-text">Proident aliquip velit qui commodo officia qui consectetur dolor ullamco
+                                    aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint
+                                    consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla
+                                    consequat velit magna.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div
+
+
             </div>
         </div>
     </div>
