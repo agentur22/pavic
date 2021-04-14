8273812b463df615bf1447453ebde40a9063a166
Drazen Pavic <pavic@agentur22.de>
2021-04-14 16:12:43 +0200

    set html layout, css styling

--- web/index.php
+++ web/index.php
@@ -1,16 +1,210 @@
 <?php
 ?>
 <html>
-    <head>
-        <title>Pavic Docker PlayGround</title>
-    </head>
-    <body>
-        <h1>
-            Kakoje Pavic, xxx
-        </h1>
-        <p>
-            Lorem idlodl ldoeor o ldeo, eodl lerooel!
-        </p>
-    </body>
+<head>
+    <title>Pavic Docker PlayGround</title>
+    <link rel="stylesheet" href="assets/css/foundation.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+       <div class="grid-container">
+<header>
+    <div class="grid-container">
+        <div class="grid-x logo">
+            <div class="cell logo-box">nu</div>
+            <div class="cell logo-text">nuntium.</div>
+        </div>
+        <nav>
+            <ul class="main-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Tags</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+        <div class="nav-right">
+            <a href="#" class="btn-login">Login</a>
+        </div>
+    </div>
+</header>
+<section>
+    <div class="grid-container">
+        <div class="hero-section">
+            <img src="assets/img/article_image.png" alt="Hero image">
+            <div class="hero-text-box">
+                <div class="article-tag">FEATURE ARTICLE</div>
+                <h2>World’s Most Dangerous Technology Ever Made.</h2>
+                <div class="article-meta">Ralph Hawkins * May 7, 2019 (10 mins read)</div>
+                <div class="article-text">Proident aliquip velit qui commodo officia qui consectetur dolor ullamco
+                    aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint
+                    consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla
+                    consequat velit magna.
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<main>
+    <div class="grid-container">
+        <div class="grid-x">
+            <div class="cell brick_news_list large-8 large-offset-2">
+                <h1>editors prick</h1>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell medium-6  large-4">
+                        <img src="assets/img/article1.png" alt="">
+                    </div>
+                    <div class="cell medium-6  large-8">
+                        <h2>
+                            Culpa sit Laboris Exercitation ea Fugiat
+                        </h2>
+                        <p>
+                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut
+                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit.
+                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
+                            ullamco labore et esse.
+                        </p>
+                    </div>
+                </div>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell medium-6 large-4">
+                        <img src="assets/img/article2.png" alt="">
+                    </div>
+                    <div class="cell medium-6 large-8">
+                        <h2>
+                            Culpa sit Laboris Exercitation ea Fugiat
+                        </h2>
+                        <p>
+                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut
+                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit.
+                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
+                            ullamco labore et esse.
+                        </p>
+                    </div>
+                </div>
+                <div class="grid-x grid-margin-x">
+                    <div class="cell medium-6 large-4">
+                        <img src="assets/img/article3.png" alt="">
+                    </div>
+                    <div class="cell medium-6 large-8">
+                        <h2>
+                            Culpa sit Laboris Exercitation ea Fugiat
+                        </h2>
+                        <p>
+                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat ut
+                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim mollit.
+                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
+                            ullamco labore et esse.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<section>
+    <div class="grid-container">
+        <div class="featured-article">
+            <img src="assets/img/article_image.png" alt="Feature image">
+            <div class="feature-text-box">
+                <div class="article-tag">FEATURE ARTICLE</div>
+                <h2>World’s Most Dangerous Technology Ever Made.</h2>
+                <div class="article-meta">Ralph Hawkins * May 7, 2019 (10 mins read)</div>
+                <div class="article-text">Proident aliquip velit qui commodo officia qui consectetur dolor ullamco
+                    aliquip elit incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint
+                    consectetur Lorem amet aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla
+                    consequat velit magna.
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="grid-container">
+        <div class="grid-x grid-margin-x ">
+            <div class="cell medium-10 large-7 large-offset-1">
+                <div class="cell grid-x">
+                    <div class="cell medium-6 large-4">
+                        <img src="assets/img/article2.png" alt="">
+                    </div>
+                    <div class="cell medium-6 large-8">
+                        <h2>
+                            Culpa sit Laboris Exercitation ea Fugiat
+                        </h2>
+                        <p>
+                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat
+                            ut
+                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim
+                            mollit.
+                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
+                            ullamco labore et esse.
+                        </p>
+                    </div>
+                </div>
+                <div class="cell grid-x">
+                    <div class="cell medium-6 large-4">
+                        <img src="assets/img/article2.png" alt="">
+                    </div>
+                    <div class="cell medium-6 large-8">
+                        <h2>
+                            Culpa sit Laboris Exercitation ea Fugiat
+                        </h2>
+                        <p>
+                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat
+                            ut
+                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim
+                            mollit.
+                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
+                            ullamco labore et esse.
+                        </p>
+                    </div>
+                </div>
+                <div class="cell grid-x">
+                    <div class="cell medium-6 large-4">
+                        <img src="assets/img/article2.png" alt="">
+                    </div>
+                    <div class="cell medium-6 large-8">
+                        <h2>
+                            Culpa sit Laboris Exercitation ea Fugiat
+                        </h2>
+                        <p>
+                            Incididunt occaecat et qui dolore consectetur magna. Lorem veniam ut et labore consequat
+                            ut
+                            ex sunt. Ut et nostrud aliquip do anim proident ad nulla consectetur eu aute ex anim
+                            mollit.
+                            Anim aute exercitation nisi fugiat. Dolor velit excepteur commodo proident nulla commodo
+                            ullamco labore et esse.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <aside class="cell medium-2 large-2 tags">
+                <h6>tags</h6>
+                <ul>
+                    <li>Technology</li>
+                    <li>Open Source</li>
+                    <li>JavaScript</li>
+                    <li>Animals</li>
+                    <li>Technology</li>
+                    <li>Open Source</li>
+                    <li>JavaScript</li>
+                    <li>Animals</li>
+                    <li>Technology</li>
+                    <li>Open Source</li>
+                    <li>JavaScript</li>
+                    <li>Animals</li>
+                </ul>
+            </aside>
+        </div>
+    </div>
+</section>
+
+
+<script src="../assets/js/vendor.js"></script>
+<script src="../assets/js/foundation.js"></script>
+       </div>
+</body>
 </html>
 
